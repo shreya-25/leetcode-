e261fb80b47410d7877a77d00da9656491ebdfc6
shreya agarwal <shreyaagarwal252000@gmail.com>
2020-09-24 09:46:43 +0530

    Create #1550. Three Consecutive Odds.java

---
+++ #1550. Three Consecutive Odds.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int c=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                c++;
+                if(c==3)
+                    return true;
+            }
+            else
+                c=0;
+        }
+        return false;
+    }
+}

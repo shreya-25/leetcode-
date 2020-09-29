c3d94a76e0e23a80d4a1f280a0ab0e6bc21ffb4b
shreya agarwal <shreyaagarwal252000@gmail.com>
2020-09-30 00:07:05 +0530

    Create #1374. Generate a String With Characters That Have Odd Counts.java

---
+++ #1374. Generate a String With Characters That Have Odd Counts.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String generateTheString(int n) {
+        String ans="";
+        if(n%2==0)
+        {
+            for(int i=1;i<=n-1;i++)
+                ans+='a';
+            ans+='b';
+        }
+        else
+        {
+            for(int i=1;i<=n;i++)
+                ans+='a';
+        }
+        return ans;
+    }
+}

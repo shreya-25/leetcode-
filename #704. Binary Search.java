fbfa1a877685a8a10a90efce1f26b29dfa24ac31
shreya agarwal <shreyaagarwal252000@gmail.com>
2020-10-26 02:19:22 +0530

    Create #704. Binary Search.java

---
+++ #704. Binary Search.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int l=0,r=nums.length-1;
+        while(l<=r)
+        {
+            int mid=(int)(l+r)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return -1;
+    }
+}

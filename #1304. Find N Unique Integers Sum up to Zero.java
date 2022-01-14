df3a02af4f7c5cf39e24785edfda4b3d8e3232f3
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2022-01-14 15:48:11 +0530

    Create #1304. Find N Unique Integers Sum up to Zero.java

---
+++ #1304. Find N Unique Integers Sum up to Zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int res[]=new int[n];
+        for(int i=0;i<n-1;i+=2)
+        {
+            res[i]=i+1;
+            res[i+1]=-(i+1);
+        }
+        return res;
+    }
+}

b2f328fa8f58078ebcc2da7652db75fba4d86797
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2022-05-18 07:56:23 +0530

    Update #463. Island Perimeter.java

--- #463. Island Perimeter.java
+++ #463. Island Perimeter.java
@@ -30,3 +30,30 @@ class Solution {
         return c;
     }
 }
+****************************************************************************************************************
+solution 2 -
+    
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int res=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    res+=4;
+                    if(i!=0 && grid[i-1][j]==1)
+                    {
+                       res-=2;
+                    }
+                    if(j!=0 && grid[i][j-1]==1)
+                    {
+                       res-=2;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
